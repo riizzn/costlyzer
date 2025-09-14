03be131d9b675aa0b898aa0c443180d6d712ff78
Rez <rezhates@gmail.com>
2025-09-14 23:17:27 +0530

    Updated Tailwind Styles

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
   return (
     <header className='w-full'>
-        <nav className='nav'></nav>
+        <nav className='nav'>
+            <Link href="/" className='flex items-center gap-1'> 
+            <Image src="/assets/icons/logo.png" width={20} height={30} alt="logo" className='object-contain'/>
+            {/* <Image src="/assets/icons/cost1.png" width={90} height={90} alt="costlyzer"/> */}
+            
+            </Link>
+            
+        </nav>
     </header>
   )
 }
